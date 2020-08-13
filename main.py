149b47c440ae8882704d664587eabb4546b267cd
Harish238 <harishvenkat238@gmail.com>
2020-08-13 15:11:46 -0500

    Add training and testing loop for Resnet

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from torchvision import datasets, transforms
 import torch
 import torch.optim as optim
 import torch.nn as nn
+from torch.optim.lr_scheduler import MultiStepLR
 
 from LeNet import LeNet
+from ResNet import Resnet
 
 
 def run_lenet(args):
@@ -77,6 +79,80 @@ def run_lenet(args):
         print("Accuracy = {}".format(100 * (correct/total)))
 
 
+def run_resnet(args):
+
+    # The Resnet paper states the following transforms are applied on the train set
+    train_set = datasets.CIFAR100("./data/", train=True, download=True, transform=transforms.Compose([
+        transforms.Normalize(  # pre-computed
+            (0.5071, 0.4865, 0.4409),
+            (0.2673, 0.2564, 0.2762)
+        ),
+        transforms.Pad(4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32),
+        transforms.ToTensor()
+    ]))
+    test_set = datasets.CIFAR100("./data/", train=False, download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(  # pre-computed
+            (0.5088, 0.4874, 0.4419),
+            (0.2683, 0.2574, 0.2771)
+        )
+    ]))
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.b, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.b, shuffle=True)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = Resnet(3).to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+    loss = nn.CrossEntropyLoss()
+
+    # learning rate should be decayed at 32k and 64k milestones
+    scheduler = MultiStepLR(optimizer, milestones=[32000, 48000], gamma=0.1)
+
+    for epoch in range(0, 64000):
+        loss_train = 0.0
+
+        for images, labels in train_loader:
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            predictions = model(images)
+            batch_loss = loss(predictions, labels)
+
+            optimizer.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+
+            loss_train += batch_loss.item()
+
+        print('{} Epoch {}, Training loss {}'.format(
+            datetime.datetime.now(),
+            epoch + 1,
+            loss_train / len(train_loader)
+        ))
+        scheduler.step()
+
+    model.eval()
+    with torch.no_grad():
+
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs, dim=1)
+            total += labels.shape[0]
+            correct += (predicted == labels).sum().item()
+
+        print("Accuracy = {}".format(100 * (correct / total)))
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -88,5 +164,5 @@ if __name__ == '__main__':
 
     if args.net == 'lenet':
         run_lenet(args)
-    # else:
-    #     run_resnet(args)
+    else:
+        run_resnet(args)
